bf91fbd7fc0fe3301fbc9dfdc7337a3563bab1e6
SantyMG03 <santymorenog2003@gmail.com>
2025-07-22 23:28:48 +0200

    Main

--- Holdem/src/Main.java
+++ Holdem/src/Main.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+
+        List<Card> playerCards = List.of(deck.draw(), deck.draw());
+        List<Card> communityCards = List.of(deck.draw(), deck.draw(), deck.draw(), deck.draw(), deck.draw());
+
+        List<Card> allCards = new ArrayList<>(playerCards);
+        allCards.addAll(communityCards);
+
+        PokerHand hand = HandEval.evaluate(allCards);
+
+        System.out.println("Tus cartas: " + playerCards);
+        System.out.println("Comunitarias: " + communityCards);
+        System.out.println("Mejor jugada: " + hand);
+    }
 }
