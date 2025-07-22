b0473722a4df04267d4c639781a837e509ddd16b
SantyMG03 <santymorenog2003@gmail.com>
2025-07-22 23:11:11 +0200

    Mano basica completo

--- Holdem/src/PokerHand.java
+++ Holdem/src/PokerHand.java
@@ -1,2 +1,30 @@
-public class PokerHand {
+import java.util.*;
+
+public class PokerHand implements Comparable<PokerHand> {
+
+    private final String handType;
+    private final List<Card> cards;
+
+    public PokerHand(String handType, List<Card> cards) {
+        this.handType = handType;
+        this.cards = cards;
+    }
+
+    public String getHandType() {
+        return handType;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    @Override
+    public int compareTo(PokerHand other) {
+        return this.handType.compareTo(other.handType);
+    }
+
+    @Override
+    public String toString() {
+        return handType + ": " + cards;
+    }
 }
