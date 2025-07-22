72cb6237eb7a2d808e6c30743649ed8b32e46644
SantyMG03 <santymorenog2003@gmail.com>
2025-07-22 23:05:40 +0200

    Mazo basico completo

--- Holdem/src/Deck.java
+++ Holdem/src/Deck.java
@@ -1,2 +1,33 @@
+import java.util.*;
+
 public class Deck {
+    private final List<Card> cards = new ArrayList<>();
+
+    /**
+     * Constructor para generar un mazo completo
+     */
+    public Deck() {
+        for (Card.Suit suit: Card.Suit.values()) {
+            for (Card.Rank rank: Card.Rank.values()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+        shuffle();
+    }
+
+    /**
+     * Metodo para barajar el mazo
+     */
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    /**
+     * Metodo para sacar la primera carta y la borra del total del mazo
+     * @return la primera carta
+     */
+    public Card draw() {
+        if (cards.isEmpty()) throw new IllegalStateException("No cards left in the deck");
+        return cards.removeFirst();
+    }
 }
