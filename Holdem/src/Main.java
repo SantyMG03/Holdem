6ef31594c671539055de37abba9d62523469f206
SantyMG03 <santymorenog2003@gmail.com>
2025-07-23 12:46:23 +0200

    Ahora se detecta poker

--- Holdem/src/Main.java
+++ Holdem/src/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Deck deck = new Deck();
 
-        List<Card> playerCards = List.of(deck.draw(), deck.draw());
-        List<Card> communityCards = List.of(deck.draw(), deck.draw(), deck.draw(), deck.draw(), deck.draw());
+        List<Card> playerCards = List.of(new Card(Card.Rank.ACE, Card.Suit.SPADES), new Card(Card.Rank.ACE, Card.Suit.CLUBS));
+        Card c1 = new Card(Card.Rank.ACE, Card.Suit.HEARTS);
+        Card c2 = new Card(Card.Rank.ACE, Card.Suit.DIAMONDS);
+        Card c3 = new Card(Card.Rank.THREE, Card.Suit.CLUBS);
+        Card c4 = new Card(Card.Rank.EIGHT, Card.Suit.DIAMONDS);
+        Card c5 = new Card(Card.Rank.TWO, Card.Suit.HEARTS);
+        List<Card> communityCards = List.of(c1,c2,c3,c4,c5);
 
         List<Card> allCards = new ArrayList<>(playerCards);
         allCards.addAll(communityCards);
