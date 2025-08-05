02dd373b945086767c42e0b29d573e2ba76de332
SantyMG03 <santymorenog2003@gmail.com>
2025-08-05 11:22:21 +0200

    Simulacion de rondas funciona

--- Holdem/src/Main.java
+++ Holdem/src/Main.java
@@ -2,29 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Deck deck = new Deck();
-
-        List<Card> player1 = List.of(deck.draw(), deck.draw());
-        List<Card> player2 = List.of(deck.draw(), deck.draw());
-        List<Card> communityCards = List.of(deck.draw(), deck.draw(), deck.draw(), deck.draw(), deck.draw());
-
-        System.out.println("Cartas comunes: "+communityCards);
-
-        List<Card> all1 = new ArrayList<>(player1);
-        all1.addAll(communityCards);
-        PokerHand hand1 = HandEval.evaluate(all1);
-
-        List<Card> all2 = new ArrayList<>(player2);
-        all2.addAll(communityCards);
-        PokerHand hand2 = HandEval.evaluate(all2);
-
-        System.out.println("Jugador 1: " +player1+ " -> " +hand1);
-        System.out.println("Jugador 2: " +player2+ " -> " +hand2);
-
-        int res = hand1.compareTo(hand2);
-
-        if (res > 0) System.out.println("Gana el jugador 1");
-        else if (res < 0) System.out.println("Gana el jugador 2");
-        else System.out.println("Empate");
+        PokerSim sim = new PokerSim(4);
+        sim.playRound();
     }
 }
--- Holdem/src/PokerSim.java
+++ Holdem/src/PokerSim.java
@@ -15,6 +15,43 @@ public class PokerSim {
     public void playRound() {
         deck.shuffle();
 
+        // Reparto de cartas privadas
         List<List<Card>> playerCards = new ArrayList<>();
+        for (int i = 0; i < numPlayers; i++) {
+            List<Card> hand = new ArrayList<>();
+            hand.add(deck.draw());
+            hand.add(deck.draw());
+            playerCards.add(hand);
+        }
+
+        // Reparto de cartas comunitarias
+        List<Card> communityCards = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            communityCards.add(deck.draw());
+        }
+
+        System.out.println("Cartas comunitarias: " + communityCards);
+
+        // Evalua manos de jugadores
+        List<PokerHand> hands = new ArrayList<>();
+        for (int i = 0; i < numPlayers; i++) {
+            List<Card> comb = new ArrayList<>(playerCards.get(i));
+            comb.addAll(communityCards);
+            PokerHand hand = HandEval.evaluate(comb);
+            hands.add(hand);
+            System.out.println("Jugador " + (i+1) + ": " + playerCards.get(i) + " -> " + hand);
+        }
+
+        // Detecta ganador
+        int winner = 0;
+        PokerHand bestHand = hands.get(0);
+        for (int i = 1; i < numPlayers; i++) {
+            if (hands.get(i).compareTo(bestHand) > 0) {
+                bestHand = hands.get(i);
+                winner = i;
+            }
+        }
+
+        System.out.println("Gana el jugadors " + (winner+1) + " con " + bestHand.getHandType());
     }
 }
